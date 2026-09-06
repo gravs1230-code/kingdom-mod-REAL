ec3b2a50e6bca14428eb8616c0109dbb645370cd
gravs1230-code <gravs1230@gmail.com>
2026-09-06 04:18:49 -0400

    Create PlayerKingdomProvider.java

---
+++ src/main/resources/META-INF/.github/workflows/src/main/java/com/example/kingdommod/src/main/java/com/example/kingdommod/src/main/java/com/example/kingdommod/kingdom/src/main/java/com/example/kingdommod/kingdom/src/main/java/com/example/kingdommod/npc/src/main/java/com/example/kingdommod/npc/src/main/java/com/example/kingdommod/npc/src/main/java/com/example/kingdommod/player/src/main/java/com/example/kingdommod/player/src/main/java/com/example/kingdommod/player/PlayerKingdomProvider.java
@@ -0,0 +1,34 @@
+package com.example.kingdommod.player;
+
+import net.minecraft.core.Direction;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import net.minecraftforge.common.util.INBTSerializable;
+import net.minecraftforge.common.util.LazyOptional;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class PlayerKingdomProvider implements ICapabilityProvider, INBTSerializable<CompoundTag> {
+    private final PlayerKingdomData data = new PlayerKingdomData();
+    private final LazyOptional<PlayerKingdomData> optional = LazyOptional.of(() -> data);
+
+    @NotNull
+    @Override
+    public <T> LazyOptional<T> getCapability(@NotNull Capability<T> cap, @Nullable Direction side) {
+        if (cap == PlayerKingdomCapability.PLAYER_DATA) {
+            return optional.cast();
+        }
+        return LazyOptional.empty();
+    }
+
+    @Override
+    public CompoundTag serializeNBT() {
+        return data.serializeNBT();
+    }
+
+    @Override
+    public void deserializeNBT(CompoundTag nbt) {
+        data.deserializeNBT(nbt);
+    }
+}
